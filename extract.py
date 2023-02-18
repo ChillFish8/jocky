9030d30ff62a5891edc52434af637566ed8a2cd8
ChillFish8 <hburt2003@gmail.com>
2023-02-18 15:30:35 +0000

    Cleanup

--- extract.py
+++
@@ -1,5 +0,0 @@
-
-with open("../datasets/data.json", encoding="utf-8") as file:
-    for i in range(0, 5):
-        line = file.readline()
-        open(f"sample-{i}.json", "w+", encoding="utf-8").write(line)
