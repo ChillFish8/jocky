5c62d539002e3e47d5e4e7757a05876e91c939cc
ChillFish8 <hburt2003@gmail.com>
2023-02-18 15:03:47 +0000

    Add write unit test

--- src/directories/writer.rs
+++ src/directories/writer.rs
@@ -1,5 +1,8 @@
 use std::collections::BTreeSet;
 use std::fmt::{Debug, Formatter};
+use std::fs::File;
+use std::io;
+use std::io::{BufWriter, Write};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
@@ -9,6 +12,7 @@ use tantivy::directory::{FileHandle, WatchCallback, WatchHandle, WritePtr};
 use tantivy::Directory;
 
 use crate::directories::IGNORE_FILES;
+use crate::metadata::SegmentMetadata;
 
 /// A writer which wraps an inner directory.
 ///
@@ -16,23 +20,66 @@ use crate::directories::IGNORE_FILES;
 /// exported into a given segment.
 pub struct DirectoryWriter<D: Directory> {
     inner: D,
-    file_path: PathBuf,
     files_to_read: Arc<Mutex<BTreeSet<PathBuf>>>,
 }
 
-impl<D: Directory> DirectoryWriter<D> {
+impl<D: Directory + Clone> DirectoryWriter<D> {
     /// Create a new directory writer.
-    pub(crate) fn new(inner: D, fp: impl AsRef<Path>) -> Self {
+    pub(crate) fn new(inner: D) -> Self {
         Self {
             inner,
             files_to_read: Default::default(),
-            file_path: fp.as_ref().to_path_buf(),
         }
     }
 
     /// The current set of files to export.
     pub fn files(&self) -> BTreeSet<PathBuf> {
         self.files_to_read.lock().clone()
+    }
+
+    pub fn write_segment(&self, fp: &Path) -> io::Result<()> {
+        #[cfg(unix)]
+        if let Some(parent) = fp.parent() {
+            File::open(parent)?.sync_data()?;
+        }
+
+        let file = File::create(fp)?;
+        let mut writer = BufWriter::new(file);
+        let mut cursor = 0;
+        let mut metadata = SegmentMetadata::default();
+
+        for file in self.files() {
+            let handle = match self.get_file_handle(&file) {
+                Ok(handle) => handle,
+                Err(OpenReadError::IoError { io_error, .. }) => {
+                    return Err(io::Error::new(io_error.kind(), io_error.to_string()))
+                },
+                _ => unimplemented!()
+            };
+
+            let file_start = cursor;
+            let bytes = handle.read_bytes(0..handle.len())?;
+            writer.write_all(&bytes)?;
+            cursor += bytes.len() as u64;
+
+            let fp = file.to_string_lossy().to_string();
+            metadata.add_file(fp, file_start..cursor);
+        }
+
+        let metadata_start = cursor;
+        let bytes = metadata.to_bytes()?;
+        writer.write_all(&bytes)?;
+        cursor += bytes.len() as u64;
+
+        crate::metadata::write_metadata_offsets(&mut writer, metadata_start, cursor)?;
+
+        writer.flush()?;
+        writer
+            .into_inner()
+            .unwrap()
+            .sync_data()?;
+
+        Ok(())
     }
 }
 
@@ -46,7 +93,6 @@ impl<D: Directory + Clone> Clone for DirectoryWriter<D> {
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
-            file_path: self.file_path.clone(),
             files_to_read: self.files_to_read.clone(),
         }
     }
@@ -81,7 +127,7 @@ impl<D: Directory + Clone> Directory for DirectoryWriter<D> {
         self.inner.atomic_read(path)
     }
 
-    fn atomic_write(&self, path: &Path, data: &[u8]) -> std::io::Result<()> {
+    fn atomic_write(&self, path: &Path, data: &[u8]) -> io::Result<()> {
         let fp = path.to_string_lossy();
         if !IGNORE_FILES.contains(&fp.as_ref()) {
             self.files_to_read.lock().insert(path.to_path_buf());
@@ -89,11 +135,86 @@ impl<D: Directory + Clone> Directory for DirectoryWriter<D> {
         self.inner.atomic_write(path, data)
     }
 
-    fn sync_directory(&self) -> std::io::Result<()> {
+    fn sync_directory(&self) -> io::Result<()> {
         self.inner.sync_directory()
     }
 
     fn watch(&self, watch_callback: WatchCallback) -> tantivy::Result<WatchHandle> {
         self.inner.watch(watch_callback)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use tantivy::collector::TopDocs;
+    use tantivy::query::QueryParser;
+    use tantivy::schema::*;
+    use tantivy::{doc, Index, IndexSettings, ReloadPolicy};
+    use tantivy::directory::MmapDirectory;
+
+    use super::*;
+
+    #[test]
+    fn test_create_segment() {
+        let dir = MmapDirectory::create_from_tempdir().unwrap();
+        let write = DirectoryWriter::new(dir);
+
+        create_segment(write.clone()).unwrap();
+
+        write.write_segment("./out.segment".as_ref()).unwrap();
+    }
+
+    fn create_segment(directory: impl Directory) -> tantivy::Result<()> {
+        let mut schema_builder = Schema::builder();
+        schema_builder.add_text_field("title", TEXT | STORED);
+        schema_builder.add_text_field("body", TEXT);
+
+        let schema = schema_builder.build();
+        let index = Index::create(directory, schema.clone(), IndexSettings::default())?;
+        let mut index_writer = index.writer(50_000_000)?;
+
+        let title = schema.get_field("title").unwrap();
+        let body = schema.get_field("body").unwrap();
+
+        index_writer.add_document(doc!(
+        title => "Of Mice and Men",
+        body => "A few miles south of Soledad, the Salinas River drops in close to the hillside \
+                bank and runs deep and green. The water is warm too, for it has slipped twinkling \
+                over the yellow sands in the sunlight before reaching the narrow pool. On one \
+                side of the river the golden foothill slopes curve up to the strong and rocky \
+                Gabilan Mountains, but on the valley side the water is lined with trees—willows \
+                fresh and green with every spring, carrying in their lower leaf junctures the \
+                debris of the winter’s flooding; and sycamores with mottled, white, recumbent \
+                limbs and branches that arch over the pool"
+        ))?;
+
+        index_writer.add_document(doc!(
+        title => "Frankenstein",
+        title => "The Modern Prometheus",
+        body => "You will rejoice to hear that no disaster has accompanied the commencement of an \
+                 enterprise which you have regarded with such evil forebodings.  I arrived here \
+                 yesterday, and my first task is to assure my dear sister of my welfare and \
+                 increasing confidence in the success of my undertaking."
+        ))?;
+
+        index_writer.commit()?;
+
+        let reader = index
+            .reader_builder()
+            .reload_policy(ReloadPolicy::OnCommit)
+            .try_into()?;
+        let searcher = reader.searcher();
+        let query_parser = QueryParser::for_index(&index, vec![title, body]);
+        let query = query_parser.parse_query("sea whale")?;
+
+        let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
+
+        for (_score, doc_address) in top_docs {
+            let retrieved_doc = searcher.doc(doc_address)?;
+            println!("{}", schema.to_json(&retrieved_doc));
+        }
+
+        Ok(())
     }
 }
